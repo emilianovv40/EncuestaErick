694310c5d3299182137492e4644df9802443b5ce
= <androvidd380@gmail.com>
2023-05-24 21:41:58 -0600

    evitar desconexión en mysql

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-    host    :   process.env.DB_HOST,
-    user    :   process.env.DB_USER,
-    password:   process.env.DB_PASS,
-    database:   process.env.DB_NAME
+    host            :   process.env.DB_HOST,
+    user            :   process.env.DB_USER,
+    password        :   process.env.DB_PASS,
+    database        :   process.env.DB_NAME,
+    connectionLimit :   0, 
+    queueLimit      :   0,
+    acquireTimeout  :   0
 })
 
 db.connect((err)=> {
